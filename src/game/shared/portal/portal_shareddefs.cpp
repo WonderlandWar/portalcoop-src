b3adca34511b07cf0d0d10527dd80f72aa65914d
Mandrew <Wonderland_War@outlook.com>
2026-03-25 14:55:54 -0500

    Remove old quickplay code

--- src/game/shared/portal/portal_shareddefs.cpp
+++ src/game/shared/portal/portal_shareddefs.cpp
@@ -74,54 +74,6 @@ void CMapInfo::Reset( void )
 #endif
 }
 
-const char *g_pszAllPcoopMaps[] =
-{
-	// Normal 2 player
-	//
-	"p2coop_0",
-	"p2coop_1",
-	"p2coop_2",
-	"p2coop_3",
-	"p2coop_4",
-	"p2coop_5",
-	"p2coop_6",
-	"p2coop_7",
-	//"p2coop_7_challenge",
-	"p2coop_8",
-	"p2coop_9",
-	"p2coop_10",
-
-	// Normal 3 player
-	//
-	"p3coop_00",
-	"p3coop_01",
-
-	// Rexaura 2 player
-	//
-	"rex2c_00",
-	"rex2c_01_destroyer_intro",
-	"rex2c_02_box_reflector_intro",
-	"rex2c_03_inf_ball_intro",
-	"rex2c_04_double_reflect",
-	"rex2c_05_field_intro",
-	"rex2c_06_portalgun_upgrade",
-	//"rex2c_06_portalgun_upgrade_advanced",
-	"rex2c_07_dissolver",
-	"rex2c_08_ball_juggle",
-	"rex2c_09_timed_catcher_intro",
-	"rex2c_10_box_deliver",
-	"rex2c_11_portal_save",
-	//"rex2c_11_portal_save_advanced",
-	//"rex2c_box_separation",
-
-	//"pcoop_playground"
-};
-
-int GetPCoopMapCount()
-{
-	return ARRAYSIZE( g_pszAllPcoopMaps );
-}
-
 CMapInfo g_MapInfo;
 
 int GetRequiredPlayers()
--- src/game/shared/portal/portal_shareddefs.h
+++ src/game/shared/portal/portal_shareddefs.h
@@ -164,10 +164,6 @@ private:
 
 extern CMapInfo g_MapInfo;
 
-extern const char *g_pszAllPcoopMaps[];
-
-int GetPCoopMapCount();
-
 int GetRequiredPlayers();
 
 bool PlayerShouldPlay( int index );
